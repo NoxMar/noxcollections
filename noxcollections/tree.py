89f0bca0782e728e0dbe377ccf94d4ad409c5dbd
Marcin Śladewski <mar4sladewski@gmail.com>
2022-07-31 16:30:02 +0200

    Add naive binary tree implementation and tests

---
+++ noxcollections/tree.py
@@ -0,0 +1,170 @@
+"""Contains ABC for readonly binary tree nodes as well reference implementation using
+references. More space-efficient implementation of the same ABC is planned for before
+the next (non-dev) release."""
+
+from abc import ABC, abstractmethod
+from collections import deque
+
+from typing import Generic, TypeVar, Optional, Generator, Tuple, Deque, Any
+
+T = TypeVar("T")
+
+
+class BinaryTreeNodeABC(Generic[T], ABC):
+    """ABC representing a readonly binary tree node.
+
+    The following properties link node to different nodes in a rather obvious way:
+    - left
+    - right
+    - parent (should be managed by the instance itself, not settable by the user)
+
+    Attributes:
+        value (T): value stored in this node
+    """
+
+    def __init__(self, value: T):
+        self.value = value
+
+    @property
+    @abstractmethod
+    def left(self) -> "Optional[BinaryTreeNodeABC[T]]":
+        """Left child of the node is any or ``None`` if node has no left child."""
+        pass
+
+    @property
+    @abstractmethod
+    def right(self) -> "Optional[BinaryTreeNodeABC[T]]":
+        """Right child of the node is any or ``None`` if node has no right child."""
+        pass
+
+    @property
+    @abstractmethod
+    def parent(self) -> "Optional[BinaryTreeNodeABC[T]]":
+        """Parent child of the node or ``None`` if this node is root."""
+
+    @property
+    def is_root(self) -> bool:
+        """``True`` if this node is a root of a binary tree (has no parent)."""
+        return self.parent is None
+
+    @property
+    def is_leaf(self) -> bool:
+        """``True`` if this node is a leaf (has no children)."""
+        return self.left is None and self.right is None
+
+    @property
+    def children(
+        self,
+    ) -> "Tuple[Optional[BinaryTreeNodeABC[T]], Optional[BinaryTreeNodeABC[T]]]":
+        """Tuple representing children of the node in the format ``(left, right)``.
+
+        If node doesn't have given child ``None`` is returned in its place.
+        """
+        return (self.left, self.right)
+
+    def traverse_bfs(self) -> "Generator[BinaryTreeNodeABC[T], None, None]":
+        """Traverses using BFS rules yielding each node as it is encountered.
+
+        Additionally, left node is yielded before the right node. This functions runs in
+        ``O(n)`` time using up to aux ``O(n)`` space.
+        You might modify yielded nodes but continuing to consume nodes
+        **after changing anything but value may lead to unexpected results**.
+
+        Yields:
+            Generator[BinaryTreeNodeABC[T], None, None]: Generator of nodes encountered
+              while traversing a tree top to bottom and on each of those levels left to
+              right.
+        """
+        to_visit: Deque[BinaryTreeNodeABC[T]] = deque()
+        to_visit.append(self)
+
+        while to_visit:
+            node = to_visit.pop()
+            yield node
+            to_visit.extendleft(n for n in node.children if n is not None)
+
+    def values_bfs(self) -> Generator[T, None, None]:
+        """Yields values from a tree in BFS order threating this node as a root.
+
+        Values from one level are yielded from the leftmost left to the rightmost.
+        This function runs in ``O(n)`` time and needs up to ``O(n)`` aux space.
+
+        Yields:
+            Generator[T, None, None]: Values from consecutive levels of a tree top to
+              bottom and on each level left to right.
+        """
+        return (n.value for n in self.traverse_bfs())
+
+    def get_level(self) -> int:
+        """Return the level of this node, treating root as having a level of 0.
+
+        Value is calculated **each time this function is called** and this process
+        might take **up to ``O(n)`` time** for the leaf of a tree that is essentially a
+        linked list.
+
+        Returns:
+            int: Level of this node (which is equivalent of the count of ancestors of
+              this node).
+        """
+        node = self
+        level = 0
+        while node.parent is not None:
+            level += 1
+            node = node.parent
+        return level
+
+    def __repr__(self) -> str:
+        return f"{self.__class__.__name__}({self.value}, {self.left}, {self.right})"
+
+    def __contains__(self, obj: Any) -> bool:
+        return obj in self.values_bfs()
+
+
+class BinaryTreeNode(BinaryTreeNodeABC[T]):
+    """Binary tree node using references to store relationship between nodes.
+
+    In contrast to its ABC this implementation allows setting the child properties.
+    Note, that this way of string binary trees is less space-efficient than the method
+    using na array.
+    """
+
+    def __init__(
+        self,
+        value: T,
+        left: "Optional[BinaryTreeNode[T]]" = None,
+        right: "Optional[BinaryTreeNode[T]]" = None,
+    ):
+        super().__init__(value)
+        self._parent: "Optional[BinaryTreeNode[T]]" = None
+
+        self._left = left
+        if self._left is not None:
+            self._left._parent = self
+
+        self._right = right
+        if self._right is not None:
+            self._right._parent = self
+
+    @property
+    def left(self) -> "Optional[BinaryTreeNode[T]]":
+        return self._left
+
+    @left.setter
+    def left(self, left: "Optional[BinaryTreeNode[T]]") -> None:
+        if left is not None:
+            left._parent = self
+        self._left = left
+
+    @property
+    def right(self) -> "Optional[BinaryTreeNode[T]]":
+        return self._right
+
+    @right.setter
+    def right(self, right: "Optional[BinaryTreeNode[T]]") -> None:
+        if right is not None:
+            right._parent = self
+        self._right = right
+
+    @property
+    def parent(self) -> "Optional[BinaryTreeNode[T]]":
+        return self._parent
---
+++ tests/test_tree.py
@@ -0,0 +1,288 @@
+"""Tests for module ``noxcollections.tree``"""
+
+from typing import Sequence, no_type_check
+import pytest
+
+from noxcollections.tree import BinaryTreeNode
+
+
+def _balanced_tree() -> BinaryTreeNode[int]:
+    """Returns the balanced tree that in BFS order returns ``0, 1, 2, 3, 4, 5, 6``
+
+    ::
+
+        0
+        |--1 (left child)
+        |  |--3  (left child)
+        |  |__4  (right child)
+        |__2  (right child)
+           |--5 (left child)
+           |__6 (right child)
+    """
+    return BinaryTreeNode(
+        0,
+        BinaryTreeNode(1, BinaryTreeNode(3), BinaryTreeNode(4)),
+        BinaryTreeNode(2, BinaryTreeNode(5), BinaryTreeNode(6)),
+    )
+
+
+@pytest.fixture
+def balanced_tree() -> BinaryTreeNode[int]:
+    """Returns the balanced tree that in BFS order returns ``0, 1, 2, 3, 4, 5, 6``
+
+    ::
+
+        0
+        |--1 (left child)
+        |  |--3  (left child)
+        |  |__4  (right child)
+        |__2  (right child)
+           |--5 (left child)
+           |__6 (right child)
+    """
+    return _balanced_tree()
+
+
+def _linked_list_tree() -> BinaryTreeNode[int]:
+    """Returns a tree degenerated to a linked list where nodes have only right children.
+
+    Consists of values ``0, 1, 2, 3, 4, 5, 6`` in top-bottom order
+
+    ::
+
+        0
+        |-x (no left child)
+        |__1 (right child)
+           |-x
+           |__2
+              |-x
+              |__3
+                 |-x
+                 |__4
+                    |-x
+                    |__5
+                       |-x
+                       |__6
+    """
+    return BinaryTreeNode(
+        0,
+        right=BinaryTreeNode(
+            1,
+            right=BinaryTreeNode(
+                2,
+                right=BinaryTreeNode(
+                    3,
+                    right=BinaryTreeNode(
+                        4, right=BinaryTreeNode(5, right=BinaryTreeNode(6))
+                    ),
+                ),
+            ),
+        ),
+    )
+
+
+def linked_list_tree() -> BinaryTreeNode[int]:
+    """Returns a tree degenerated to a linked list where nodes have only right children.
+
+    Consists of values ``0, 1, 2, 3, 4, 5, 6`` in top-bottom order
+
+    ::
+
+        0
+        |-x (no left child)
+        |__1 (right child)
+           |-x
+           |__2
+              |-x
+              |__3
+                 |-x
+                 |__4
+                    |-x
+                    |__5
+                       |-x
+                       |__6
+    """
+    return _linked_list_tree()
+
+
+def test_binary_tree_node_constructor_sets_value():
+    node = BinaryTreeNode("TEST")
+
+    assert node.value == "TEST"
+
+
+def test_binary_tree_node_setting_value_changes_value():
+    node = BinaryTreeNode(1)
+    node.value = -1
+
+    assert node.value == -1
+
+
+def test_binary_tree_node_one_arg_constructor_creates_a_node_without_children():
+    node = BinaryTreeNode(1)
+
+    assert node.left is None
+    assert node.right is None
+
+
+def test_binary_tree_node_without_parent_is_root():
+    assert BinaryTreeNode(1).is_root
+
+
+def test_binary_tree_node_with_a_parent_is_not_root():
+    leaf = BinaryTreeNode(2)
+    root = BinaryTreeNode(1, leaf)  # noqa: needed to a parent for leaf
+
+    assert not leaf.is_root
+
+
+def test_binary_tree_node_without_children_is_leaf():
+    assert BinaryTreeNode(1).is_leaf
+
+
+def test_binary_tree_node_with_children_is_not_leaf():
+    assert not BinaryTreeNode(1, BinaryTreeNode(2)).is_leaf
+
+
+def test_binary_tree_node_sets_relationship_with_the_left_child_correctly():
+    left_child = BinaryTreeNode(2)
+    root = BinaryTreeNode(1, left_child)
+
+    assert root.left is left_child
+    assert left_child.parent is root
+
+
+def test_binary_tree_node_sets_relationship_with_the_right_child_correctly():
+    right_child = BinaryTreeNode(2)
+    root = BinaryTreeNode(1, None, right_child)
+
+    assert root.right is right_child
+    assert right_child.parent is root
+
+
+def test_binary_tree_node_setting_left_child_sets_the_relationship_properly():
+    left_child = BinaryTreeNode(2)
+    root = BinaryTreeNode(1)
+    root.left = left_child
+
+    assert root.left is left_child
+    assert left_child.parent is root
+
+
+def test_binary_tree_node_setting_right_child_sets_the_relationship_properly():
+    right_child = BinaryTreeNode(2)
+    root = BinaryTreeNode(1)
+    root.right = right_child
+
+    assert root.right is right_child
+    assert right_child.parent is root
+
+
+def test_binary_tree_node_children_should_be_consistent_with_properties_for_a_leaf():
+    assert BinaryTreeNode(1).children == (None, None)
+
+
+def test_binary_tree_node_children_should_be_correct_for_the_left_child_only():
+    left_child = BinaryTreeNode(2)
+    node = BinaryTreeNode(1, left_child)
+
+    assert node.children == (left_child, None)
+
+
+def test_binary_tree_node_children_should_be_correct_for_the_right_child_only():
+    left_child = BinaryTreeNode(0)
+    right_child = BinaryTreeNode(2)
+    node = BinaryTreeNode(1, left_child, right_child)
+
+    assert node.children == (left_child, right_child)
+
+
+def test_binary_tree_node_children_should_be_correct_for_node_with_both_children():
+    right_child = BinaryTreeNode(2)
+    node = BinaryTreeNode(1, right=right_child)
+
+    assert node.children == (None, right_child)
+
+
+def test_binary_tree_node_root_get_level_returns_0(balanced_tree: BinaryTreeNode[int]):
+    assert balanced_tree.get_level() == 0
+
+
+@no_type_check  # Since checking for None at each level would obscure the test
+def test_binary_tree_node_get_heigh_return_value_consistent_with_number_of_its_parents(
+    balanced_tree: BinaryTreeNode[int],
+):
+    # level 1
+    assert balanced_tree.left.get_level() == balanced_tree.right.get_level() == 1
+
+    # Level 2
+    assert (
+        balanced_tree.left.left.get_level()
+        == balanced_tree.left.right.get_level()
+        == balanced_tree.right.left.get_level()
+        == balanced_tree.right.right.get_level()
+        == 2
+    )
+
+
+@no_type_check  # Since checking for None at each level would obscure the test
+def test_binary_tree_node_more_complex_structures_is_formed_correctly(
+    balanced_tree: BinaryTreeNode[int],
+):
+    # Check root
+    assert balanced_tree.value == 0
+
+    # Check root children
+    assert balanced_tree.left.value == 1
+    assert balanced_tree.right.value == 2
+
+    # Check children of the root's left child
+    assert balanced_tree.left.left.value == 3
+    assert balanced_tree.left.right.value == 4
+
+    # Check children of the root's right child
+    assert balanced_tree.right.left.value == 5
+    assert balanced_tree.right.right.value == 6
+
+
+@pytest.mark.parametrize("tree", [_balanced_tree(), _linked_list_tree()])
+def test_binary_tree_node_traverse_bfs_returns_all_nodes_in_a_tree(
+    tree: BinaryTreeNode[int],
+):
+    assert set(n.value for n in tree.traverse_bfs()) == set(range(7))
+
+
+@pytest.mark.parametrize(
+    ("tree", "order"),
+    [
+        (_balanced_tree(), [0, 1, 2, 3, 4, 5, 6]),
+        (_linked_list_tree(), [0, 1, 2, 3, 4, 5, 6]),
+    ],
+)
+def test_binary_tree_bfs_traverse_returns_values_in_a_correct_order(
+    tree: BinaryTreeNode[int], order: Sequence[int]
+):
+    assert list(n.value for n in tree.traverse_bfs()) == order
+
+
+@pytest.mark.parametrize("tree", [_balanced_tree(), _linked_list_tree()])
+def test_binary_tree_bfs_values_should_be_consistent_with_values_of_traverse_bfs(
+    tree: BinaryTreeNode[int],
+):
+    assert list(n.value for n in tree.traverse_bfs()) == list(tree.values_bfs())
+
+
+@pytest.mark.parametrize("tree", [_balanced_tree(), _linked_list_tree()])
+@pytest.mark.parametrize("to_find", range(7))
+def test_binary_tree_bfs_contains_should_return_true_if_value_is_in_tree(
+    tree: BinaryTreeNode[int], to_find: int
+):
+    assert to_find in tree
+
+
+@pytest.mark.parametrize("tree", [_balanced_tree(), _linked_list_tree()])
+@pytest.mark.parametrize("to_find", [-1, "abc"])
+def test_binary_tree_bfs_contains_should_return_false_if_value_is_not_in_tree(
+    tree: BinaryTreeNode[int], to_find: object
+):
+    assert to_find not in tree
